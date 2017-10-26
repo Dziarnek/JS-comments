b319441f13a4485cec58a19bd6e64a70fd4ad6dc
Dziarnek <kuba.dziarnowski@onet.eu>
2017-10-26 06:03:13 +0200

    Adding comments in script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 var name = prompt('Wpisz swoje imię');
-alert('Witaj ' + name);
+// alert('Witaj ' + name);
 console.log('Witaj ' + name);
+
+//This line has been commented manually by adding "//" at the beginning
+
+// And this one by key combination (ctrl + /)
